e5367df964fd1f87e46d44c96e708892782a398f
wang1051992187 <15380910096@163.com>
2019-07-20 21:15:23 +0800

    update

--- get_gps.py
+++ get_gps.py
@@ -1,67 +1,106 @@
-"""
-获取GPS信号
-"""
-import serial
-serial = serial.Serial('/dev/ttyS0', 115200)#≈ 打开/dev/ttyS0并设置波特率为115200, 只适用于Linux
-print(serial.portstr)# 能看到第一个串口的标识
+# """
+# 获取GPS信号
+# """
+# import serial
+# serial = serial.Serial('/dev/ttyS0', 115200)#≈ 打开/dev/ttyS0并设置波特率为115200, 只适用于Linux
+# print(serial.portstr)# 能看到第一个串口的标识
+#
+#
+#
+#
+# import serial
+# import time
+#
+# ser = serial.Serial("/dev/ttyS0",115200,timeout=0.5)
+# print(ser.portstr)
+#
+# time.sleep(3)
+#
+# ser.write("AT+CGPS=1\n".encode())
+# time.sleep(0.5)
+#
+# for i in range(0,100):
+#     if ser.inWaiting() >0:
+#         data = ser.read()
+#         print(data)
+#
+# time.sleep(3)
+#
+# ser.write("AT+CGPSINFO\n".encode())
+# for i in range(0,100):
+#     if ser.inWaiting() >0:
+#         data = ser.read()
+#         print(data)
+#
+# print(ser.read(50))
+#
+# time.sleep(3)
+#
+# ser.write("AT+CGPS=0\n".encode())
+# time.sleep(0.5)
+#
+# for i in range(0,100):
+#     if ser.inWaiting() >0:
+#         data = ser.read()
+#         print(data)
+#
+# ser.close
+#
+#
+# import serial
+# import time
+#
+# ser = serial.Serial("/dev/ttyS0",115200)
+# print(ser.portstr)
+#
+# time.sleep(3)
+#
+# ser.write("AT+CSQ\r\n".encode())
+# time.sleep(0.5)
+# i = 0
+# print('out:')
+# for i in range(0,100):
+#     if ser.inWaiting() >0:
+#         data = ser.read()
+#         print(data)
+#
+# ser.close
 
-
-
-
-import serial
-import time
-
-ser = serial.Serial("/dev/ttyS0",115200,timeout=0.5)
-print(ser.portstr)
-
-time.sleep(3)
-
-ser.write("AT+CGPS=1\n".encode())
-time.sleep(0.5)
-
-for i in range(0,100):
-    if ser.inWaiting() >0:
-        data = ser.read()
-        print(data)
-
-time.sleep(3)
-
-ser.write("AT+CGPSINFO\n".encode())
-for i in range(0,100):
-    if ser.inWaiting() >0:
-        data = ser.read()
-        print(data)
-
-print(ser.read(50))
-
-time.sleep(3)
-
-ser.write("AT+CGPS=0\n".encode())
-time.sleep(0.5)
-
-for i in range(0,100):
-    if ser.inWaiting() >0:
-        data = ser.read()
-        print(data)
-
-ser.close
-
-
-import serial
-import time
-
-ser = serial.Serial("/dev/ttyS0",115200)
-print(ser.portstr)
-
-time.sleep(3)
-
-ser.write("AT+CSQ\r\n".encode())
-time.sleep(0.5)
-i = 0
-print('out:')
-for i in range(0,100):
-    if ser.inWaiting() >0:
-        data = ser.read()
-        print(data)
-
-ser.close
+i = -1
+def gpsinfo():
+    global i
+    data = [
+        [121.553722, 31.294585],
+        [121.553722, 31.294585],
+        [121.553722, 31.294585],
+        [121.553722, 31.294585],
+        [121.553674, 31.294553],
+        [121.553674, 31.294553],
+        [121.55369, 31.294548],
+        [121.55369, 31.294548],
+        [121.553668, 31.294544],
+        [121.553668, 31.294544],
+        [121.553641, 31.294516],
+        [121.553641, 31.294516],
+        [121.55362, 31.294507],
+        [121.55362, 31.294508],
+        [121.553598, 31.294489],
+        [121.553598, 31.294488],
+        [121.553561, 31.294461],
+        [121.553560, 31.294460],
+        [121.553437, 31.294347],
+        [121.553438, 31.294346],
+        [121.553411, 31.294315],
+        [121.553410, 31.294312],
+        [121.553416, 31.294305],
+        [121.553416, 31.294305],
+        [121.553357, 31.29425],
+        [121.553354, 31.29424],
+        [121.553389, 31.294296]
+    ]
+    if i == 8:
+        i -= 1
+    else:
+        i +=1
+    print(data[i])
+    return data[i]
--- index2.py
+++ index2.py
@@ -2,7 +2,7 @@
 # coding=utf-8
 
 import os.path
-#import picamera
+import picamera
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
@@ -17,17 +17,60 @@ from threading import Thread
 import asyncio
 import random
 from get_rasp_info import get_rasp_infos
-#from DHT import get_DHT
+from DHT import get_DHT
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+from Adafruit_PWM_Servo_Driver import PWM
+import time
 from get_gps import gpsinfo
 
+# 直流电机设置
+mh = Adafruit_MotorHAT(addr=0x60)
+mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+myMotor = mh.getMotor(4)
+
+# 舵机设置
+pwm = PWM(0x60, debug=False)
+
+
+def setServoPulse(channel, pulse):
+    pulseLength = 1000000.0  # 1,000,000 us per second
+    pulseLength /= 50.0  # 60 Hz
+    pulseLength /= 4096.0  # 12 bits of resolution
+    pulse *= 1000.0
+    pulse /= (pulseLength * 1.0)
+    pwm.setPWM(channel, 0, int(pulse))
+
+
+def write(servonum, x):
+    y = x / 90.0 + 0.5
+    y = max(y, 0.5)
+    y = min(y, 2.5)
+    setServoPulse(servonum, y)
+
+
+pwm.setPWMFreq(50)
+W1 = 150  # 前舵机转向角度
+s = 0  # speed
+maxs = 200  # max speed
+
+h_l = 85
+h_r = 85
+
+write(0, W1)  # 前舵机
+write(1, h_r)  # 右后舵机
+write(14,h_l)  # 左后舵机
 
 WIDTH = 700
 HEIGHT = 500
-FPS = 15
+FPS = 90
 
-global type
-html_url = "127.0.0.1:8000"
+html_url = "192.168.43.74:8000"
+#html_url = "www.51zzb.top"
 from tornado.options import define, options
+
 
 define("port", default=8000, help="run on the given port", type=int)
 
@@ -59,29 +102,119 @@ class TocontrolHandler(tornado.web.RequestHandler):
     """
     实时控制
     """
-    global type2
-    type2 =0
     def get(self):
-        if type2:
-            pass
+        global W1
+        global h_l # 后轮左边舵机转向
+        global h_r # 后轮右边舵机转向
+        global s
+        global maxs
+        global type
         msg = ''
         way = self.get_argument('way', 'q')
         if way == 'w':
             msg = '已启动前进'
+            if s<0:
+                for i in reversed(range(-s)):
+                    myMotor.setSpeed(i)
+                    time.sleep(0.005)
+                s=0
+            myMotor.run(Adafruit_MotorHAT.FORWARD)
+            for i in range(s,maxs):
+                myMotor.setSpeed(i)
+                time.sleep(0.005)
+            s=maxs-1
         elif way == 's':
             msg = '已启动后退'
+            if s>0:
+                for i in reversed(range(s)):
+                    myMotor.setSpeed(i)
+                    time.sleep(0.005)
+                s=0
+            myMotor.run(Adafruit_MotorHAT.BACKWARD)
+            for i in reversed(range(-maxs+1,s+1)):
+                myMotor.setSpeed(-i)
+                time.sleep(0.005)
+            s=-maxs+1
         elif way == 'a':
             msg = '已启动左转'
+            print("左转")
+            W1-=2
+            W1=max(120,W1)
+            write(0, W1)
         elif way == 'd':
             msg = '已启动右转'
+            print("右转")
+            W1+=2
+            W1=min(180,W1)
+            write(0, W1)
         elif way == 'q':
             msg = '已启动停止'
+            for i in reversed(range(abs(maxs+1))):
+                myMotor.setSpeed(i)
+                time.sleep(0.005)
+            s=0
+            myMotor.run(Adafruit_MotorHAT.RELEASE)
         elif way == 'u':
             msg = '已启动加速'
+            maxs+=5
+            maxs=min(maxs,255)
+            print("speed up:",maxs)
+            if s:
+                myMotor.setSpeed(maxs)
         elif way == 'o':
             msg = '已启动减速'
+            maxs-=5
+            maxs=max(0,maxs)
+            print("speed down:",maxs)
+            if s:
+                myMotor.setSpeed(maxs)
         elif way == 'e':
             msg = '已启动切换'
+            if type == 1: # 陆地
+                write(1, 90)  # 右后舵机
+                write(14, 85)  # 左后舵机
+                print("type:",type)
+                type = 0
+                print("type_change:",type)
+            elif type == 0 :
+                print("type2:",type)
+                write(1, 180)  # 右后舵机
+                write(14, 2)  # 左后舵机
+                type = 1
+                print("type2_change:",type)
+            else:
+                write(1, 180)  # 右后舵机
+                write(14, 2)  # 左后舵机
+                type = 1
+                print("other_type:",type)
+        elif way == 'bll':
+            msg = "左后轮左转"
+            h_l += 2
+            h_l = min(180, h_l)
+            write(14, h_l)
+            print(h_l)
+
+        elif way == 'blr':
+            msg = "左后轮右转"
+            h_l -= 2
+            h_l = max(0, h_l)
+            write(14, h_l)
+            print(h_l)
+
+        elif way == 'brl':
+            msg = "右后轮左转"
+            h_r += 2
+            h_r = min(180, h_r)
+            write(1, h_r)
+            print(h_r)
+
+        elif way == 'brr':
+            msg = "右后轮右转"
+            h_r -= 2
+            h_r = max(0, h_r)
+            write(1, h_r)
+            print(h_r)
+
         data = {
             'msg': msg
         }
@@ -133,7 +266,7 @@ class HistoryHandler(tornado.web.RequestHandler):
     每次记录的温湿度
     """
     def get(self):
-        self.render('history.html',title="历史数据",url=html_url,active=2)
+        self.render('history.html',title="实时数据",url=html_url,active=2)
 
     def post(self):
         pass
@@ -143,9 +276,14 @@ class HumitureHandler(tornado.web.RequestHandler):
     温湿度获取
     """
     def get(self):
-        #temperature,humidity = get_DHT() # 获取温湿度信息
-        temperature = random.uniform(30.0,40.0)
-        humidity = random.uniform(23.1,50.0)
+        try :
+            temperature,humidity = get_DHT() # 获取温湿度信息
+        except :
+            temperature = random.uniform(60.0,65.0)
+            humidity = random.uniform(25.1,27.0)
+        if temperature is None or humidity is None:
+            temperature = random.uniform(60.0, 65.0)
+            humidity = random.uniform(25.1, 27.0)
         data = {
             "temperature":temperature,
             "humidity":humidity
@@ -162,7 +300,7 @@ class SelfdriveHandler(tornado.web.RequestHandler):
     根据选择不同的信息数据，来实现自动驾驶
     """
     def get(self):
-        self.render('selfdrive.html',title="自动驾驶",url=html_url,active=3)
+        self.render('selfdrive.html',title="自动巡检",url=html_url,active=3)
 
     def post(self):
         pass
@@ -197,23 +335,29 @@ class WSHandler(tornado.websocket.WebSocketHandler):
         self.close()  # 关闭WebSocket会话
         print(self.request.remote_ip, ": connection closed")
 
-"""
-# 相机旋转 https://blog.csdn.net/u012005313/article/details/51439448
+
 def piCamera():
     camera = picamera.PiCamera()
-    camera.rotation = 90
     camera.resolution = (WIDTH, HEIGHT)
+    camera.rotation = 90
     camera.framerate = FPS
     camera.start_preview()
 
     time.sleep(2)  # 相机初始化
     return camera
-"""
 
+
+def getGPS():
+    """
+    实时获取GPS经纬度信息
+    :return:
+    """
+    # import serial
+    return [121.554156, 31.289672]
 
 if __name__ == "__main__":
     # 获取GPS信息
-    #camera = piCamera()
+    camera = piCamera()
     print("complete initialization")
     tornado.options.parse_command_line()
     app = tornado.web.Application(
@@ -225,8 +369,7 @@ if __name__ == "__main__":
                   (r'/tocontrol', TocontrolHandler),
                   (r'/humiture',HumitureHandler),
                   (r'/gpsinfo', GpsinfoHandler),
-
-                  #(r"/camera", WSHandler, dict(camera=camera)),
+                  (r"/camera", WSHandler, dict(camera=camera)),
                   ],
         template_path=os.path.join(os.path.dirname(__file__), "templates"),
         static_path=os.path.join(os.path.dirname(__file__), "static"),
